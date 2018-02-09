39aedd845e5d66f69bb9ac792c0d477ad3c97054
Keith Manning <Keith.Manning@Findel-Education.co.uk>
2018-02-09 14:10:28 +0000

    Created WorksheetNotFoundException class to be thrown when trying to retrieve data that doesn't exist in the Excel sheet.

---
+++ ExcelDataReader/Exceptions/WorksheetNotFoundException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelDataReader.Exceptions
+{
+    [Serializable]
+    public class WorksheetNotFoundException : Exception
+    {
+
+        public WorksheetNotFoundException() { }
+
+        public WorksheetNotFoundException(string message) : base(message) { }
+
+        public WorksheetNotFoundException(string message, Exception innerException) : base(message, innerException) { }
+
+        protected WorksheetNotFoundException(SerializationInfo info, StreamingContext ctxt) : base(info, ctxt) { }
+
+
+    }
+}

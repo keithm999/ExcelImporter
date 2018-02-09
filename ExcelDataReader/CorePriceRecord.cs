7dd9286a77f03b6ff527b23e6cd83621de4c9428
Keith Manning <Keith.Manning@Findel-Education.co.uk>
2018-02-09 14:08:11 +0000

    Created CorePriceRecord class to handle importing of core price records from Excel spreadsheet.

--- ExcelDataReader/CorePriceRecord.cs
+++ ExcelDataReader/CorePriceRecord.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ExcelDataReader.Controllers;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +10,7 @@ using CoreGroup = System.String;
 
 namespace ExcelDataReader
 {
-    class CorePriceRecord
+    public class CorePriceRecord
     {
         #region Properties
         private Brand _brand;
@@ -30,6 +33,56 @@ namespace ExcelDataReader
 
         public CorePriceRecord()
         {
+            _material = new Material();
+            _customer = new Customer();
+            
+        }
+
+        // Constructor for when passing a DataRow from the DataTable on the CorePrices tab
+        public CorePriceRecord(DataRow row) : base()
+        {
+            _material = new Material();
+            _customer = new Customer();
+
+            if (row == null)
+            {
+                Debug.WriteLine("DataRow is null, creating blank CorePriceRecord object");
+            }
+            else
+            {
+                if(row.Table.Columns.Count != FileController.CoreSheetColumnCount)
+                {
+                    Debug.WriteLine("Column count is wrong, found {0} but was expecting {1}", row.Table.Columns.Count, FileController.CoreSheetColumnCount);
+                    throw new IndexOutOfRangeException(String.Format("Column count is wrong, found {0} but was expecting {1}", row.Table.Columns.Count, FileController.CoreSheetColumnCount));
+                }
+
+                // Parse the row data
+                Brand = (Brand)Brand.Parse(typeof(Brand), row[FileController.CoreBrandHeaderText].ToString());
+                Material.ItemCode = row[FileController.CoreMaterialHeaderText].ToString();
+                Material.CataloguePrice = float.Parse(row[FileController.CoreStandardPriceHeaderText].ToString());
+                Material.CorePrice = float.Parse(row[FileController.CoreCorePriceHeaderText].ToString());
+                Customer.AccountNumber = row[FileController.CoreCustomerHeaderText].ToString();
+                Customer.CoreGroup = row[FileController.CoreCoreGroupHeaderText].ToString();
+            }
+
+
+        }
+
+
+        public override string ToString()
+        {
+            //return base.ToString();
+
+            string retString = "";
+            retString += "Brand: " + _brand;
+            retString += ", Material: " + _material.ItemCode;
+            retString += ", Customer: " + _customer.AccountNumber;
+            retString += ", Core Group: " + _customer.CoreGroup;
+            retString += ", Standard Price: " + _material.CataloguePrice;
+            retString += ", Core Price: " + _material.CorePrice;
+
+
+            return retString;
         }
     }
 }
